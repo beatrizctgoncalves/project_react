b92db5a2c302b8f1dba6212ea8bbf1c2fc14664c
Miguel <pinto.miguel6@gmail.com>
2021-04-28 17:57:28 +0100

    started implementing connection to gitlab

--- server/src/api-gitlab.js
+++ server/src/api-gitlab.js
@@ -4,57 +4,51 @@ const URL = 'https://gitlab.com/api/v4/'
 
 const fetch = require('node-fetch');
 
+/*Test to get issues*/
 getUserId(UserName)
     .then(Uid => getProjects(Uid,AccessToken))
     .then(Pid => getIssues(Pid,AccessToken))
 
 
-
-function getUserId(username) {
-    return fetch(`${URL}users?username=${username}`, {
+function makeRequest(URI){
+    return fetch(`${URL}${URI}`, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
         }
     })
     .then(response => response.json())
-    .then(user => user[0].id)
     .catch(err => console.error(err));
+}
+
+
+function getUserId(username) {
+    return makeRequest(`users?username=${username}`)
+        .then(user => user[0].id)
+        .catch(err => console.error(err));
 }
 
 
 function getProjects(Uid,AToken) {
-    return fetch(`${URL}users/${Uid}/projects?access_token=${AToken}`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(projects => projects[0].id)
-    .catch(err => console.error(err));
+    return makeRequest(`users/${Uid}/projects?access_token=${AToken}`)
+        .then(projects => projects[0].id)  //getting the first project available, should be searched by name or something similar
+        .catch(err => console.error(err));
 }
 
 function getIssues(PId,AToken) {
-    return fetch(`${URL}projects/${PId}/issues?access_token=${AToken}`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    .then(response => response.json())
-    .then(issues => issues.map(issue => {
-        return {
-            "id" : issue.id, 
-            "title" : issue.title, 
-            "state": issue.state,
-            "closed_at":issue.closed_at,
-            "assignees":issue.assignees.map(assignee => assignee.username),
-            "upvotes": issue.upvotes,
-            "downvotes": issue.downvotes,
-            "due_date": issue.due_date 
-        }
-    }))
-    .then(issues => console.log(issues))
-    .catch(err => console.error(err));
+    return makeRequest(`projects/${PId}/issues?access_token=${AToken}`)
+        .then(issues => issues.map(issue => {
+            return {
+                "id" : issue.id, 
+                "title" : issue.title, 
+                "state": issue.state,
+                "closed_at":issue.closed_at,
+                "assignees":issue.assignees.map(assignee => assignee.username),
+                "upvotes": issue.upvotes,
+                "downvotes": issue.downvotes,
+                "due_date": issue.due_date 
+            }
+        }))
+        .then(issues => console.log(issues))
+        .catch(err => console.error(err));
 }
