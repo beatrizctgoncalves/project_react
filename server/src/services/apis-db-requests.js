723796a236b8dea0f3c2653eb859d338e89c16f5
Miguel <pinto.miguel6@gmail.com>
2021-06-01 20:24:51 +0100

    implemented modules gitlab and Jira(missing)

--- server/src/services/apis-db-requests.js
+++ server/src/services/apis-db-requests.js
@@ -8,7 +8,6 @@ function requests(fetch, pgResponses) {
         },
 
         ES_URL :'http://localhost:9200/',
-        GIT_URL : 'https://gitlab.com/api/v4/',
 
         makeFetchElastic: function(uri, method, body) {
             return fetch(`${ES_URL}`.concat(uri), {
@@ -19,36 +18,6 @@ function requests(fetch, pgResponses) {
                 body: body //Request body
             })
                 .then(response => response.json()).catch(err=>console.log(err)) //Expecting json response
-        },
-
-        makeFetchJira: function(uri, method, body, email, token) {
-            return fetch(uri, {
-                method: method,
-                headers: {
-                    'Authorization': `Basic ${Buffer.from(
-                        `${email}:${token}`
-                    ).toString('base64')}`,
-                    'Accept': 'application/json'
-                },
-                body: body //Request body
-            })
-                .then(response => {
-                    if(response.status != pgResponses.OK) return Promise.reject(response);
-                    return response.json()
-                })
-        },
-
-        makeRequestGitLab: function(URI) {
-            return fetch(this.GIT_URL.concat(URI), {
-                method: this.arrayMethods.GET,
-                headers: {
-                    'Accept': 'application/json'
-                }
-            })
-            .then(response => {
-                if(response.status != pgResponses.OK) return Promise.reject(response);
-                return response.json()
-            })
         }
     }
     return serv;
