9fbe377726d504fbbd09dafd31c1ddc2f9e92705
Miguel <pinto.miguel6@gmail.com>
2021-06-15 21:06:12 +0100

    finished ScoreCounting in Jira

--- server/src/services/plugins/Jira/ScoreCounter.js
+++ server/src/services/plugins/Jira/ScoreCounter.js
@@ -2,18 +2,17 @@
 
 module.exports = {
     countPoints: function(Pid, userInfoMap, owner, beginDate, endDate) {
-        const ApiGitlab = require("./api")()
+        const ApiJira = require("./api")()
         let AToken = userInfoMap.get(owner).info
-        AToken = AToken.filter(info => info.type == "Gitlab")[0].AToken
-        let memberInfoMapGitlab = new Map()
+        AToken = AToken.filter(info => info.type == "Jira")[0].AToken
+        let memberInfoMapJira = new Map()
         Array.from(userInfoMap, ([key, value]) => {
             value.info.forEach(info => {
-                if(info.type == "Gitlab")
-                    memberInfoMapGitlab.set(info.username, {"AppUsername" : key, "Points" : 0 })
+                if(info.type == "Jira")
+                    memberInfoMapJira.set(info.accountId, {"AppUsername" : key, "Points" : 0 })
             })
         })
-
-        return ApiGitlab.getIssues(Pid,AToken)
+        return ApiJira.getIssues(Pid,AToken)
             .then(issues => {
                 issues.forEach(issue => {
                     if(checkDate(issue.created_at, beginDate, endDate)){
@@ -22,16 +21,14 @@ module.exports = {
                             Points += 10
                         if(issue.due_date && issue.closed_at && issue.closed_at.slice(0,10)>issue.due_date)
                             Points -=5
-                        issue.assignees.forEach(assignee => {
-                            if(memberInfoMapGitlab.has(assignee)){
-                                let aux = memberInfoMapGitlab.get(assignee)
-                                aux.Points += Points
-                                memberInfoMapGitlab.set(assignee,aux)
-                            }
-                        })
+                        if(memberInfoMapJira.has(issue.assigneeId)){
+                            let aux = memberInfoMapJira.get(issue.assigneeId)
+                            aux.Points += Points
+                            memberInfoMapJira.set(issue.assigneeId,aux)
+                        }
                     }
                 })
-                return memberInfoMapGitlab
+                return memberInfoMapJira
             })
     }
 }
